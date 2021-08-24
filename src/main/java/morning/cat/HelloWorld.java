fc947726b187c15d2fab5534f095d6fac03153f6
morningcat <morningcat2018@qq.com>
2021-08-25 01:45:19 +0800

    v0

---
+++ src/main/java/morning/cat/HelloWorld.java
@@ -0,0 +1,17 @@
+package morning.cat;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @describe: 类描述信息
+ * @author: morningcat.zhang
+ * @date: 2021/8/25 上午1:43
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        new ConcurrentHashMap<Object, Object>();
+        LocalDateTime localDateTime = LocalDateTime.now();
+    }
+}
