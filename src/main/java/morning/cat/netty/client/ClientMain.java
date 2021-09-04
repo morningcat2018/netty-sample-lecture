c62155abc549ac6aabeb46be26166fdd888d15dc
morningcat <morningcat2018@qq.com>
2021-09-05 04:54:56 +0800

    start

--- src/main/java/morning/cat/netty/client/ClientMain.java
+++ src/main/java/morning/cat/netty/client/ClientMain.java
@@ -3,11 +3,11 @@ package morning.cat.netty.client;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import morning.cat.netty.client.handle.EchoClientHandler;
 
-import java.util.Scanner;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @describe: 类描述信息
@@ -16,56 +16,55 @@ import java.util.Scanner;
  */
 public class ClientMain {
 
-    public static void main(String[] args) throws InterruptedException {
+    private static final int MAX_RETRY = 16;
+
+    public static void main(String[] args) {
         // 用来处理I/O操作的多线程事件循环器
         EventLoopGroup workerGroup = new NioEventLoopGroup();
-
         try {
-
             // 客户端
-            Bootstrap clientBootstrap = new Bootstrap();
-
-            // 设置 NioSocket 工厂
-            clientBootstrap.group(workerGroup);
-            clientBootstrap.channel(NioSocketChannel.class);
-
-
-            // 设置管道工厂
-            clientBootstrap.handler(new ChannelInitializer<Channel>() {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(workerGroup);
+            bootstrap.channel(NioSocketChannel.class);
+            bootstrap.handler(new ChannelInitializer<Channel>() {
                 @Override
                 protected void initChannel(Channel channel) throws Exception {
-
                     ChannelPipeline channelPipeline = channel.pipeline();
                     channelPipeline.addLast("ClientHandle", new EchoClientHandler());
                 }
             });
-
             // 设置参数
-            clientBootstrap.option(ChannelOption.SO_BACKLOG, 128);
+            bootstrap.option(ChannelOption.SO_BACKLOG, 128);
 
             // 连接服务端
-            ChannelFuture channelFuture = clientBootstrap.connect("127.0.0.1", 51001).sync();
+            ChannelFuture channelFuture = connect(bootstrap, "127.0.0.1", 51001, MAX_RETRY);
             System.out.println("Netty4 Client start...");
-
-
             //channelFuture.channel().closeFuture().sync();
 
-//            Channel channel = channelFuture.channel();
-//            Scanner scanner = new Scanner(System.in);
-//            while (true) {
-//                System.out.print("请输入：");
-//                String ss = scanner.next();
-//                if (ss.equals("exit")) {
-//                    break;
-//                }
-//                channel.writeAndFlush(ss);
-//            }
-
-
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         } finally {
             // 关闭资源
             workerGroup.shutdownGracefully();
         }
+    }
+
+    private static ChannelFuture connect(Bootstrap bootstrap, String host, int port, int retry) throws InterruptedException {
+        ChannelFuture channelFuture = bootstrap.connect(host, port).addListener(future -> {
+            if (future.isSuccess()) {
+                System.out.println("连接成功!");
+            } else if (retry == 0) {
+                System.err.println("重试次数已用完，放弃连接！");
+            } else {
+                // 第几次重连
+                int order = (MAX_RETRY - retry) + 1;
+                // 本次重连的间隔
+                int delay = 1 << order;
+                System.err.println(new Date() + ": 连接失败，第" + order + "次重连……");
+                bootstrap.config().group().schedule(() -> connect(bootstrap, host, port, retry - 1), delay, TimeUnit.SECONDS);
+            }
+        });
+        return channelFuture;
     }
 }
 
--- src/main/java/morning/cat/netty/server/ServerMain.java
+++ src/main/java/morning/cat/netty/server/ServerMain.java
@@ -4,69 +4,95 @@ import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.codec.string.StringDecoder;
-import io.netty.handler.codec.string.StringEncoder;
+import io.netty.util.AttributeKey;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
 import morning.cat.netty.server.handle.EchoServerHandle;
+
 
 public class ServerMain {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		// 用来处理I/O操作的多线程事件循环器
-		EventLoopGroup bossGroup = new NioEventLoopGroup(1);
-		EventLoopGroup workerGroup = new NioEventLoopGroup();
-		try {
-			// 服务端
-			ServerBootstrap serverBootstrap = new ServerBootstrap();
+        // 用来处理I/O操作的多线程事件循环器
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            // 服务端
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
 
-			// 设置 NioSocket 工厂
-			serverBootstrap.group(bossGroup, workerGroup);
-			serverBootstrap.channel(NioServerSocketChannel.class);
-			// 设置管道工厂 ChannelHandler
-			serverBootstrap.childHandler(new ChannelInitializer<Channel>() {
-				@Override
-				protected void initChannel(Channel channel) throws Exception {
+            // 设置 NioSocket 工厂
+            serverBootstrap.group(bossGroup, workerGroup);
+            serverBootstrap.channel(NioServerSocketChannel.class);
 
-					ChannelPipeline channelPipeline = channel.pipeline();
-					// 接收信息转换成string(上行)
-					// channelPipeline.addLast("StringDecoder", new StringDecoder());
-					// 回写直接写入字符串
-					// channelPipeline.addLast("StringEncoder", new StringEncoder());
+            // attr()方法可以给服务端的 channel，也就是NioServerSocketChannel指定一些自定义属性，
+            // 然后我们可以通过channel.attr()取出这个属性
+            serverBootstrap.attr(AttributeKey.newInstance("serverName"), "nettyServer");
+            serverBootstrap.childAttr(AttributeKey.newInstance("clientKey"), "clientValue");
 
-					//
-					// channelPipeline.addLast("EchoServerHandle", new EchoServerHandle());
-					//
-					// channelPipeline.addLast("HelloHandle", new HelloHandle());
+            // bossGroup handle
+            // 指定在服务端启动过程中的一些逻辑
+            serverBootstrap.handler(new ChannelInitializer<NioServerSocketChannel>() {
+                protected void initChannel(NioServerSocketChannel ch) {
+                    System.out.println("服务端启动中");
+                }
+            });
 
-					//
-					channelPipeline.addLast("HelloHandle", new EchoServerHandle());
+            // workerGroup handle
+            // 指定处理新连接数据的读写处理逻辑
+            serverBootstrap.childHandler(new ChannelInitializer<Channel>() {
+                @Override
+                protected void initChannel(Channel channel) throws Exception {
 
-					// channelPipeline.addLast("TimeServerHandler", new TimeServerHandler());
-				}
-			});
-			// 设置参数
-			serverBootstrap.option(ChannelOption.SO_BACKLOG, 128);
-			serverBootstrap.childOption(ChannelOption.SO_KEEPALIVE, true);
+                    ChannelPipeline channelPipeline = channel.pipeline();
+                    // 接收信息转换成string(上行)
+                    // channelPipeline.addLast("StringDecoder", new StringDecoder());
+                    // 回写直接写入字符串
+                    // channelPipeline.addLast("StringEncoder", new StringEncoder());
 
-			// 绑定端口
-			// telnet 127.0.0.1 51001
-			// Bind and start to accept incoming connections.
-			ChannelFuture channelFuture = serverBootstrap.bind(51001).sync();
-			System.out.println("Netty4 Server start...");
+                    //
+                    // channelPipeline.addLast("EchoServerHandle", new EchoServerHandle());
+                    //
+                    // channelPipeline.addLast("HelloHandle", new HelloHandle());
 
-			// Wait until the server socket is closed.
-			// In this example, this does not happen, but you can do that to gracefully
-			// shut down your server.
-			channelFuture.channel().closeFuture().sync();
-			System.out.println("closeFuture");
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			// 关闭资源
-			workerGroup.shutdownGracefully();
-			bossGroup.shutdownGracefully();
-		}
-	}
+                    //
+                    channelPipeline.addLast("HelloHandle", new EchoServerHandle());
+
+                    // channelPipeline.addLast("TimeServerHandler", new TimeServerHandler());
+                }
+            });
+            // 设置参数
+            serverBootstrap
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .childOption(ChannelOption.TCP_NODELAY, true)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            // 绑定端口
+            // telnet 127.0.0.1 51001
+            ChannelFuture channelFuture = bind(serverBootstrap, 51001);
+            //System.out.println("Netty4 Server start...");
+            channelFuture.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            // 关闭资源
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+
+    private static ChannelFuture bind(final ServerBootstrap serverBootstrap, final int port) {
+        ChannelFuture channelFuture = serverBootstrap.bind(port).addListener(new GenericFutureListener<Future<? super Void>>() {
+            public void operationComplete(Future<? super Void> future) {
+                if (future.isSuccess()) {
+                    System.out.println("端口[" + port + "]绑定成功!");
+                } else {
+                    System.err.println("端口[" + port + "]绑定失败!");
+                    bind(serverBootstrap, port + 1);
+                }
+            }
+        });
+        return channelFuture;
+    }
 
 }
