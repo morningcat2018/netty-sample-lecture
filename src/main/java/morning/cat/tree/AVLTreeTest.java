c204576447d072f47f11944b409e01cc93c1c57c
morningcat <morningcat2018@qq.com>
2021-11-08 02:17:27 +0800

    AVL树

--- src/main/java/morning/cat/tree/AVLTreeTest.java
+++ src/main/java/morning/cat/tree/AVLTreeTest.java
@@ -9,25 +9,24 @@ public class AVLTreeTest {
 
     public static void main(String[] args) {
         AVLTree bbt = new AVLTree();
-        bbt.put(3);
-        bbt.put(2);
-        bbt.put(1);
-        bbt.put(4);
-        bbt.put(5);
-        bbt.put(6);
-        bbt.put(7);
+        bbt.put(17);
+        bbt.put(20);
         bbt.put(10);
-        bbt.put(9);
+        bbt.put(19);
+        bbt.put(30);
+        bbt.put(23);
+        bbt.put(36);
+        bbt.put(2);
         bbt.middOrderErgodic();
 
-        System.out.println("-----各节点平衡状况-----");
-        bbt.sequenceErgodic();
-
-        bbt.delete(5);
-        bbt.delete(2);
-        bbt.middOrderErgodic();
-
-        System.out.println("-----各节点平衡状况-----");
-        bbt.sequenceErgodic();
+//        System.out.println("-----各节点平衡状况-----");
+//        bbt.sequenceErgodic();
+//
+//        bbt.delete(5);
+//        bbt.delete(2);
+//        bbt.middOrderErgodic();
+//
+//        System.out.println("-----各节点平衡状况-----");
+//        bbt.sequenceErgodic();
     }
 }
